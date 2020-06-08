a139cbb5d53248a827874561ca5e7b685aa18e36
1Cbondy1 <claytonbondy@gmail.com>
2020-06-07 21:39:52 -0500

    move vehicles data to separate file

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,62 +1,7 @@
 import React from 'react';
 import Animation from './Animation';
 
-var vehicles = [
-    {
-        id: 1,
-        type: 'Small Sedan',
-        maintenancePerMile: 0.0853,
-        depreciationPerMile: 0.1493,
-    },
-    {
-        id: 2,
-        type: 'Medium Sedan',
-        maintenancePerMile: 0.0918,
-        depreciationPerMile: 0.2113,
-    },
-    {
-        id: 3,
-        type: 'Large Sedan',
-        maintenancePerMile: 0.0949,
-        depreciationPerMile: 0.2707,
-    },
-    {
-        id: 4,
-        type: 'Small SUV',
-        maintenancePerMile: 0.0909,
-        depreciationPerMile: 0.2088,
-    },
-    {
-        id: 5,
-        type: 'Medium SUV',
-        maintenancePerMile: 0.0960,
-        depreciationPerMile: 0.2529,
-    },
-    {
-        id: 6,
-        type: 'Minivan',
-        maintenancePerMile: 0.0873,
-        depreciationPerMile: 0.2690,
-    },
-    {
-        id: 7,
-        type: 'Pickup Truck',
-        maintenancePerMile: 0.0877,
-        depreciationPerMile: 0.2464,
-    },
-    {
-        id: 8,
-        type: 'Hybrid',
-        maintenancePerMile: 0.0770,
-        depreciationPerMile: 0.2058,
-    },
-    {
-        id: 9,
-        type: 'Electric',
-        maintenancePerMile: 0.0660,
-        depreciationPerMile: 0.3500,
-    },
-]
+import { vehicles } from '../data/vehicles.js'
 
 class Main extends React.Component {
     state = {
