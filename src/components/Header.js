dfcc2f6d126bc0413b96582f2a671c4155dd89d8
1Cbondy1 <claytonbondy@gmail.com>
2020-06-18 20:19:03 -0500

    add info trigger and update fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
 class Header extends React.Component {
+    state = {
+        hidden: true
+    };
+
+    handleClick = () =>
+        this.setState(
+            prevState => ({ hidden: !prevState.hidden })
+        );
 
     render() {
         return (
             <header> 
                 <div className="title-section">
                     <h1>RIDESHARE COST CALCULATOR</h1>
-                    <p className="disclaimer">
-                        *This tool is intended to calculate the true cost of operating your vehicle for a rideshare service. Calculations account for fuel, maintenance, repairs, and depreciation costs. Auto insurance, licensing, registration, taxes, and financing are not included. All values assume 15,000 miles annually and are sourced from
+                    <div className={`trigger ${!this.state.hidden && "info-on"}`} onClick={() => this.handleClick()}>
+                        Info<span>▼</span>
+                    </div>
+                    <p className={`disclaimer ${!this.state.hidden && "info-on"}`}>
+                        This tool is intended to calculate the true cost of operating your vehicle for a rideshare service. Calculations account for fuel, maintenance, repairs, and depreciation costs. Auto insurance, licensing, registration, taxes, and financing are not included. 
+                        <br/>
+                        All values assume 15,000 miles annually and are sourced from
                         <a href="https://exchange.aaa.com/automotive/driving-costs/#.XtwAYJ5Kiu4"> https://exchange.aaa.com/automotive/driving-costs/#.XtwAYJ5Kiu4</a>
+                        <br/>
+                        Animation Credit: @MohamedIrfan <a href="https://lottiefiles.com/irfanzx">https://lottiefiles.com/irfanzx</a>
                     </p>
                 </div>
             </header>
